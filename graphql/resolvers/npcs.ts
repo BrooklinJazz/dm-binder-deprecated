c3bc2e3411527894e7ce126ab2e0f0fbc63beb9c
Brooklin Myers <Brooklinjazz@hotmail.com>
2019-09-29 13:03:03 -0600

    refactore createNpc using toObject and spead syntax

--- graphql/resolvers/npcs.ts
+++ graphql/resolvers/npcs.ts
@@ -1,7 +1,7 @@
-import Npc from '../../models/npcs';
-import { IContext, IInput, INpc, INpcInput } from '../../models/types';
-import User from '../../models/users';
-import { userFromId, checkSignedIn } from './helpers';
+import Npc from "../../models/npcs";
+import { IContext, IInput, INpc, INpcInput } from "../../models/types";
+import User from "../../models/users";
+import { userFromId, checkSignedIn } from "./helpers";
 
 export default {
   Query: {
@@ -35,17 +35,12 @@ export default {
         const createdNpc = await npc.save();
         const dbUser = await User.findById(userId);
         if (!dbUser) {
-          throw new Error('User Does Not Exist');
+          throw new Error("User Does Not Exist");
         }
         dbUser.npcs.push(createdNpc);
         dbUser.save();
-        // NOTE for some reason using spread syntax
-        // makes name and description keys undefined
-        // I've posted a question on S/O: https://stackoverflow.com/questions/58126454/using-spread-syntax-with-mongoose-document-after-calling-the-save-method-result
         return {
-          ...createdNpc,
-          description: createdNpc.description,
-          name: createdNpc.name,
+          ...createdNpc.toObject(),
           creator: userFromId(npc.creator)
         };
       } catch (error) {
